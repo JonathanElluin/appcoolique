c1b8cac896d42655c640ac48e70d9d32ecfeb1d8
RBG74 <remy.bernardgranger@gmail.com>
2018-06-04 21:28:15 +0200

    Begining of the add form.

--- src/pages/appcoolique-add/appcoolique-add.ts
+++ src/pages/appcoolique-add/appcoolique-add.ts
@@ -1,25 +1,18 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AppcooliqueAddPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Alcoolique } from "../../models/appcoolique";
 
 @IonicPage()
 @Component({
-  selector: 'page-appcoolique-add',
-  templateUrl: 'appcoolique-add.html',
+  selector: "page-appcoolique-add",
+  templateUrl: "appcoolique-add.html"
 })
 export class AppcooliqueAddPage {
+  alcoolique: Alcoolique = new Alcoolique();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AppcooliqueAddPage');
+    console.log("ionViewDidLoad AppcooliqueAddPage");
   }
-
 }
